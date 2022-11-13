5c4001113648a89e262ed0b8a90bf2f4a736d74b
Nuno Levezinho <nuno.levezinho@live.com.pt>
2022-11-13 23:20:45 +0000

    Added route to disassociate pet from owner
    - Updated owner get route to get more details from pets

--- src/controllers/ownerController.ts
+++ src/controllers/ownerController.ts
@@ -6,16 +6,108 @@ import {Cat} from "../models/cat";
 import {Dog} from "../models/dog";
 
 function findAll(req: Request, res: Response) {
-    res.send(Database.owners);
+
+    const mappedOwners = Database.owners.map(owner => {
+        const mappedPets = owner.pets.map(pet => {
+            if(pet.type === "cat") {
+                const findCat = Database.cats.find(cat => cat.id === pet.id);
+                if (!findCat) {
+                    return null;
+                }
+                return {
+                    id: findCat.id,
+                    name: findCat.name,
+                    age: findCat.age,
+                    weight: findCat.weight,
+                    sex: findCat.sex,
+                    color: findCat.color,
+                    ownerId: findCat.ownerId,
+                    type: pet.type
+                };
+            }
+
+            if(pet.type === "dog") {
+                const findDog = Database.dogs.find(dog => dog.id === pet.id);
+                if (!findDog) {
+                    return null;
+                }
+                return {
+                    id: findDog.id,
+                    name: findDog.name,
+                    age: findDog.age,
+                    weight: findDog.weight,
+                    sex: findDog.sex,
+                    color: findDog.color,
+                    ownerId: findDog.ownerId,
+                    type: pet.type
+                };
+            }
+
+            return null;
+        })
+       return {
+           id: owner.id,
+           name: owner.name,
+           pets: mappedPets
+       };
+    });
+
+    res.send(mappedOwners);
 }
 
 function findOne(req: Request, res: Response) {
 
     const { id } = req.params;
 
-    const result = Database.owners.find(owner => owner.id === id);
+    const existingOwner = Database.owners.find(owner => owner.id === id);
+    if (!existingOwner) {
+        res.send(`Owner with id ${id} not found`);
+        return;
+    }
 
-    res.send(result);
+    const mappedPets = existingOwner.pets.map(pet => {
+        if(pet.type === "cat") {
+            const findCat = Database.cats.find(cat => cat.id === pet.id);
+            if (!findCat) {
+                return null;
+            }
+            return {
+                id: findCat.id,
+                name: findCat.name,
+                age: findCat.age,
+                weight: findCat.weight,
+                sex: findCat.sex,
+                color: findCat.color,
+                ownerId: findCat.ownerId,
+                type: pet.type
+            };
+        }
+
+        if(pet.type === "dog") {
+            const findDog = Database.dogs.find(dog => dog.id === pet.id);
+            if (!findDog) {
+                return null;
+            }
+            return {
+                id: findDog.id,
+                name: findDog.name,
+                age: findDog.age,
+                weight: findDog.weight,
+                sex: findDog.sex,
+                color: findDog.color,
+                ownerId: findDog.ownerId,
+                type: pet.type
+            };
+        }
+
+        return null;
+    });
+
+    res.send({
+        id: existingOwner.id,
+        name: existingOwner.name,
+        pets: mappedPets
+    });
 }
 
 function create(req: Request, res: Response) {
@@ -138,7 +230,78 @@ function  associateDog (dog: Dog, owner: Owner, res: Response) {
 }
 
 function disassociate (req: Request, res: Response) {
+    const {id, petId} = req.params;
 
+    const existingOwner = Database.owners.find(owner => owner.id === id);
+
+    if (!existingOwner) {
+        res.send('Owner not found');
+        return;
+    }
+    const existingCat = Database.cats.find(cat => cat.id === petId);
+    const existingDog = Database.dogs.find(dog => dog.id === petId);
+
+    if (!existingCat && !existingDog) {
+        res.send(`Pet with id ${petId} not found`);
+        return;
+    }
+
+    if(existingCat) {
+        disassociateCat(existingCat, existingOwner, res);
+        return;
+    }
+
+    if(existingDog) {
+        disassociateDog(existingDog, existingOwner, res);
+        return;
+    }
+
+}
+
+function disassociateCat (cat: Cat, owner: Owner, res: Response) {
+    if(cat.ownerId !== '' && cat.ownerId !== null) {
+        cat.ownerId = '';
+
+        if (!remotePetFromOwner(cat.id, owner)) {
+            res.send(`Pet not found`);
+            return;
+        }
+
+        res.send('Pet disassociated');
+        return;
+    }
+
+    res.send('Pet isn\'t associated')
+}
+
+function disassociateDog (dog: Dog, owner: Owner, res: Response) {
+    if(dog.ownerId !== '' && dog.ownerId !== null) {
+        dog.ownerId = '';
+
+        if (!remotePetFromOwner(dog.id, owner)) {
+            res.send(`Pet not found`);
+            return;
+        }
+
+        res.send('Pet disassociated');
+        return;
+    }
+
+    res.send('Pet isn\'t associated')
+}
+
+function remotePetFromOwner(petId: string, owner: Owner) {
+
+    const existingPet = owner.pets.find(pet => pet.id === petId);
+
+    if (!existingPet) {
+        return false;
+    }
+
+    const index = owner.pets.indexOf(existingPet);
+    owner.pets.splice(index, 1);
+
+    return true;
 }
 
 export const ownersController = {
